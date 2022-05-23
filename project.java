032a58f9339d6405d617e9b3b4f95c34b2b4ebbf
yantaozhu <102832758+yantaozhu@users.noreply.github.com>
2022-05-23 00:54:57 -0400

    Update project.java

--- project.java
+++ project.java
@@ -2,163 +2,196 @@ import java.util.*;
 import java.io.*;
 
 public class Interpreter {
-
-    private String s;
-    private int index;
-    private char inputChar;
-    private HashMap<String, Integer> map = new HashMap<String, Integer>();
-    
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         try {
-            Scanner sc = new Scanner(new File("test.txt"));
-            Interpreter evalExp = new Interpreter();
-            evalExp.run(sc);
+            String input;
+            String[] variable = new int[10];
+            String[] value = new int[10];
+            int count = 0, pos = -1, result;
+            Scanner sc = new Scanner(new File("input1.txt"));
+            while (sc.hasNextLine()) {
+                input = sc.nextLine().trim();
+                basicCheck(input);
+                for (int i = 0; i < input.length();i++){
+                    if (input.charAt(i) == "=") {
+                        if(Character.isDigit(s.charAt(i+1))) {
+                            if(s.charAt(i+1) != 'O' || s.charAt(i+2) != '0') {
+                                System.out.println(input);
+                                variable[count] = input.substring(0,i-1);
+                                value[count] = input.substring(i+1,input    .length()-2);
+                                count++;
+                                break;
+                            }
+                            else{
+                                throw new RuntimeException("error");
+                            }
+                        }
+                        else if(Character.isLetter(s.charAt(i+1)) && noOperation(input.substring(i+1,input.length()-2))) {
+                            for (int i = 0; i < variable.length; i++){
+                                if(variable[i] == input.substring(i+1,input.length()-2)) {
+                                    pos = i;
+                                }
+                            }
+                            if (pos != -1){
+                                System.out.println(input.substring(0,i-1) + value[pos]);
+                                variable[count] = input.substring(0,i-1);
+                                value[count] = value[pos];
+                                pos = -1;
+                                count++;
+                                break;
+                            }
+                            else{
+                                throw new RuntimeException("error");
+                            }
+                        }
+                        else {
+                            result = doMath(input.substring(i+1,input.length()-2),variable,value);
+                            System.out.println(input.substring(0,i-1) + result);
+                            variable[count] = input.substring(0,i-1);
+                            value[count] = Integer.toString(result);
+                            count++;
+                            break
+                        }
+                    }
+                }
+            }
         }
         catch (Exception e) {
             System.out.println(e);
         }
     }
-    
-    public void run(Scanner sc) {
-        while (sc.hasNextLine()) {
-            removeSpace(sc.nextLine());
-            assignment();
-        }
-    }
-
-    void removeSpace(String s) {
-        this.s = s.replaceAll("\\s", "");
-        index = 0;
-        nextChar();
-    }
-
-    void nextChar(){
-        char c;
-        if(!s.endswith(';')) {
-            throw new RuntimeException("error");
-        }
-        c = s.charAt(index++);
-        inputChar = c;
-    }
-
-    void assignment() {
-        String var = identifier();
-        int operand = eval();
-        map.put(var, operand);
-        System.out.println(var + " = " + operand);
-    }
-
-    String identifier(){
-        StringBuilder sb = new StringBuilder();
-
-        if (Character.isLetter(inputChar)) {
-            sb.append(inputChar);
-        }
-        else
-            throw new RuntimeException("error");
-        nextChar();
-
-        while (Character.isLetter(inputChar) || Character.isDigit(inputChar) || inputChar == '_' ) {
-            sb.append(inputChar);
-            nextChar();
-        }
-
-        if (inputChar != '=') {
-            throw new RuntimeException("error");
-        }
-        nextChar();
-        return sb.toString();
-    }
-
-    int eval() {
-        int x = exp();
-        if (inputChar == ';') {     
-            return x;
-        } else {
+    public static void basiCheck(String s){
+        if ((s.charAt(s.length()-1) != ';') || (Character.isDigit(s.charAt(0)))){
             throw new RuntimeException("error");
         }
     }
-
-    int exp() {
-        int t = term();
-        while (inputChar == '+' || inputChar == '-') {
-            char inputChar = inputChar;
-            nextChar();
-            int t2 = term();
-            t = apply(inputChar, t, t2);
+    public static boolean noOperation(String s){
+        String letter;
+        boolean result;
+        for (int i = 0; i < s.length(); i++) {
+            letter = s.charAt(i);
+            switch (letter) {
+                case '+': result = false;
+                    break;
+                case '-': result = false;
+                    return false;
+                    break;
+                case '*': result = false;
+                    return false;
+                    break;
+                case '/': result = false;
+                    break;
+                default: break;
+            }
         }
-        return t;
+        return result;
     }
-
-    int term() {
-        int f = factor();
-        while (inputChar == '*' || inputChar == '/') {
-            char inputChar = inputChar;
-            nextChar();
-            int f2 = factor();
-            f = apply(inputChar, f, f2);
+    public static int doMath(String s, String[] x, String[] y) {
+        Stack<Integer> stack=new Stack<>();
+        String s1, s2;
+        int val1, val2;
+        postfix = infixToPostfix(s);
+        for(int i = 0; i < postfix.length();i++)
+        {
+            char c = postfix.charAt(i);
+            if(Character.isLetterOrDigit(c))
+                stack.push(c);
+            else
+            {
+                s1 = stack.pop();
+                if (Character.isDigit(val1)) {
+                    val1 = parseInt(s1);
+                }
+                else {
+                    for (int i = 0; i < x.length; i++) {
+                        if (s1 == x[i]) {
+                            val1 = parseInt(y[i]);
+                            break;
+                        }
+                        if (i == (x.length - 1)) {
+                            throw new RuntimeException("error");
+                        }
+                    }
+                }
+                s2 = stack.pop();
+                if (Character.isDigit(val2)) {
+                    val2 = parseInt(s2);
+                }
+                else {
+                    for (int i = 0; i < x.length; i++) {
+                        if (s2 == x[i]) {
+                            val2 = parseInt(y[i]);
+                            break;
+                        }
+                        if (i == (x.length - 1)) {
+                            throw new RuntimeException("error");
+                        }
+                    }
+                }
+                switch(c)
+                {
+                    case '+':
+                    stack.push(val2+val1);
+                    break;
+                     
+                    case '-':
+                    stack.push(val2- val1);
+                    break;
+                     
+                    case '/':
+                    stack.push(val2/val1);
+                    break;
+                     
+                    case '*':
+                    stack.push(val2*val1);
+                    break;
+              }
+            }
         }
-        return f;
+        return stack.pop();
     }
-
-    int factor() {
-        int x = 0;
-        String temp = String.valueOf(inputChar);
-        if (map.containsKey(temp)) {
-            x = map.get(temp).intValue();
-            nextChar();
-            return x;
-        } else if (inputChar == '(') {
-            nextChar();
-            x = exp();
-            match(')');
-            return x;
-        } else if (inputChar == '-') {
-            nextChar();
-            x = factor();
-            return -x;
-        } else if (inputChar == '+') {
-            nextChar();
-            x = factor();
-            return x;
-        } else if (inputChar == '0') {
-            nextChar();
-            if (Character.isDigit(inputChar))
+    public static String infixToPostfix(String s) {
+        String result = "";
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i<s.length(); ++i) {
+            char c = s.charAt(i);
+            if (Character.isLetterOrDigit(c)) {
+                result = result + c;
+            }
+            else if (c == '(') {
+                stack.push(c);
+            }
+            else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(')
+                    result = result + stack.pop();
+                    stack.pop();
+            }
+            else {
+                while (!stack.isEmpty() && precedence(c) <= precedence(stack.peek())){
+                    result = result + stack.pop();
+                }
+                stack.push(c);
+            }
+        }
+        while (!stack.isEmpty()){
+            if(stack.peek() == '('){
                 throw new RuntimeException("error");
-            return 0;
-        }
-        temp = "";
-        while (Character.isDigit(inputChar)) {
-            temp += inputChar;
-            nextChar();
-        }
-        return Integer.parseInt(temp);
+            }
+            result = result + stack.pop();
+         }
+        return result;
     }
-
-    void match(char token) {
-        if (inputChar == token) {
-            nextChar();
-        } else {
-            throw new RuntimeException("error");
-        }
-    }
-
-    static int apply(char ch, int t, int t2) {
-        int t3 = 0;
-        switch (ch) {
+    public static int precedence(char c){
+        switch (c){
             case '+':
-                t3 = t + t2;
-                break;
             case '-':
-                t3 = t - t2;
-                break;
+                return 1;
             case '*':
-                t3 = t * t2;
-                break;
             case '/':
-                t3 = t / t2;
-                break;
+                return 2;
+            case '^':
+                return 3;
         }
-        return t3;
+        return -1;
     }
 }
